0c3f2e89c6b34075e9eac20d9d49c5dfc3d748a3
rui-watanabe <wataru610806@gmail.com>
2019-09-26 16:12:20 +0900

    Initial Commit

---
+++ Swift5MapAndProtocol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5MapAndProtocol
+//
+//  Created by watar on 2019/09/26.
+//  Copyright © 2019 rui watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
